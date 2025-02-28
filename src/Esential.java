71d848ee50dc26727e114a28c6984b3088c21385
Ali 🫡 <c4000133@newcastle.ac.uk>
2025-02-28 01:00:49 +0000

    Adding fileds for Esential.java and adding setter and getter for each filed

--- src/Esential.java
+++ src/Esential.java
@@ -1,2 +1,29 @@
 public class Esential {
+    private String victimeIP;
+    private String victimeName;
+    private String hostIP;
+
+    public Esential() {}
+    public Esential(String victimeIP, String victimeName, String hostIP) {}
+
+    public String getVictimeIP() {
+        return victimeIP;
+    }
+    public void setVictimeIP(String victimeIP) {
+        this.victimeIP = victimeIP;
+    }
+    public String getVictimeName() {
+        return victimeName;
+    }
+    public void setVictimeName(String victimeName) {
+        this.victimeName = victimeName;
+    }
+    public String getHostIP() {
+        return hostIP;
+    }
+    public void setHostIP(String hostIP) {
+        this.hostIP = hostIP;
+    }
+
+
 }
